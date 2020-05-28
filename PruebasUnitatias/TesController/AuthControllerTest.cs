953ef675c2797bba2cc04e657e9bc99bd8465aa9
GIANMARCO SEGUNDO TINTINAPON MUNOZ <N00035539@upn.pe>
2020-05-28 00:00:04 -0500

    examen

---
+++ PruebasUnitatias/TesController/AuthControllerTest.cs
@@ -0,0 +1,50 @@
+﻿using BibliotecaUPN.Web.Controllers;
+using BibliotecaUPN.Web.Interface;
+using BibliotecaUPN.Web.Models;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace PruebasUnitatias.TesController
+{
+    [TestFixture]
+    public class AuthControllerTest
+    {
+        [Test]
+        public void DebePoderIngresarUsuario()
+        {
+            string Username = "admin";
+            string Password = "admin";
+
+            Usuario usuario = new Usuario()
+            {
+                Id = 1,
+                Nombres = "Ana",
+                Password = "admin",
+                Username = "admin"
+            };
+
+            var login = new Mock<IUsuarioSession>();
+            var login2 = new Mock<IUsuario>();
+
+            login2.Setup(p => p.GetUsuario(Username, Password)).Returns(usuario);
+
+            login.Setup(a => a.AutenticaUsername(Username, true));
+
+            var controller = new AuthController(login2.Object, login.Object);
+
+            
+            var rederit = controller.Login(Username, Password);
+
+            Assert.IsInstanceOf<RedirectToRouteResult>(rederit);
+            Assert.IsNotInstanceOf<ViewResult>(rederit);
+
+        }
+    
+    }
+}
---
+++ PruebasUnitatias/TesController/BibliotecaControllerTest.cs
@@ -0,0 +1,107 @@
+﻿using BibliotecaUPN.Web.Controllers;
+using BibliotecaUPN.Web.Interface;
+using BibliotecaUPN.Web.Models;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace PruebasUnitatias.TesController
+{
+    [TestFixture]
+    public class BibliotecaControllerTest
+    {
+       [Test]
+       public void ListaDeBibliotecas()
+        {
+            var sess = new Mock<IUsuario>();
+            var list = new Mock<IBiblioteca>();
+            Usuario usuario = new Usuario()
+            {
+                Id = 1,
+                Nombres = "Anda",
+                Password = "Admin",
+                Username = "Admin"
+            };
+
+            sess.Setup(s => s.setNombreUsuario()).Returns(usuario);
+            list.Setup(a => a.GetBibliotecas(usuario)).Returns(new List<Biblioteca>());
+
+            var listaBi = new BibliotecaController(list.Object, sess.Object);
+
+            var view = listaBi.Index() as ViewResult;
+
+            Assert.IsInstanceOf<List<Biblioteca>>(view.Model);
+            Assert.IsInstanceOf<ViewResult>(view);
+        }
+        [Test]
+        public void AddBiblioteca()
+        {
+            var sess = new Mock<IUsuario>();
+            var list = new Mock<IBiblioteca>();
+            Usuario usuario = new Usuario()
+            {
+                Id = 1,
+                Nombres = "Anda",
+                Password = "Admin",
+                Username = "Admin"
+            };
+
+            sess.Setup(s => s.setNombreUsuario()).Returns(usuario);
+            list.Setup(a => a.GetBibliotecas(usuario)).Returns(new List<Biblioteca>());
+
+            var listaBi = new BibliotecaController(list.Object, sess.Object);
+
+            var view = listaBi.Add(1);
+            Assert.IsInstanceOf<RedirectToRouteResult>(view);
+        }
+        [Test]
+        public void MarcarComoLeyendo()
+        {
+            var sess = new Mock<IUsuario>();
+            var list = new Mock<IBiblioteca>();
+            Usuario usuario = new Usuario()
+            {
+                Id = 1,
+                Nombres = "Anda",
+                Password = "Admin",
+                Username = "Admin"
+            };
+
+            sess.Setup(s => s.setNombreUsuario()).Returns(usuario);
+            list.Setup(a => a.GetBibliotecas(usuario)).Returns(new List<Biblioteca>());
+
+            var listaBi = new BibliotecaController(list.Object, sess.Object);
+
+            var view = listaBi.MarcarComoLeyendo(1); 
+            Assert.IsInstanceOf<RedirectToRouteResult>(view);
+        }
+        [Test]
+        public void MarcarComoTerminado()
+        {
+            var sess = new Mock<IUsuario>();
+            var list = new Mock<IBiblioteca>();
+            Usuario usuario = new Usuario()
+            {
+                Id = 1,
+                Nombres = "Anda",
+                Password = "Admin",
+                Username = "Admin"
+            };
+
+            sess.Setup(s => s.setNombreUsuario()).Returns(usuario);
+            list.Setup(a => a.GetBibliotecas(usuario)).Returns(new List<Biblioteca>());
+
+            var listaBi = new BibliotecaController(list.Object, sess.Object);
+
+            var view = listaBi.MarcarComoTerminado(1);
+            Assert.IsInstanceOf<RedirectToRouteResult>(view);
+        }
+        
+    }
+    
+}
